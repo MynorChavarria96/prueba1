03c6d5823339e9641b2f67392a6cdb88232eddaa
Mynor Estuardo Chavarría Pérez <mchavarriap@miumg.edu.gt>
2021-02-19 23:04:25 -0600

    Iniciando proyecto

---
+++ Chavarria/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chavarria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
